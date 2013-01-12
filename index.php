1d9752bcd0e4f64c36a07a868a80d4708532645d
Jeck <i@jeck.ru>
2013-01-12 20:29:22 +0400

    Заглушка на главной странице

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-  return 'Hello '.$app->escape($name);
+$app->get('/', function () {
+  return 'uGrabber main page';
 });
 
 $app->run();
