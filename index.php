40eea2a5d996bfbf84eeb0f72ca6afecd33c33f6
Jeck <i@jeck.ru>
2013-01-13 18:07:23 +0400

    каркас интерфеса на twig

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
+
 require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-  return 'uGrabber main page';
+require_once __DIR__.'/config.php';
+
+$app->register(new \Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    $twig->addGlobal('base_url', $app['config.baseUrl']);
+
+    return $twig;
+}));
+
+$app->get('/', function () use ($app) {
+	return $app['twig']->render('index.html');
 });
 
 $app->run();
